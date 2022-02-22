fb25de6477e9a3bffabf9ed325b9fb96078a331d
amimulst <st158750@stud.uni-stuttgart.de>
2022-02-22 23:34:29 +0100

    fontawesome added

--- src/main/java/com/ami/main/controller/HomeController.java
+++ src/main/java/com/ami/main/controller/HomeController.java
@@ -1,14 +1,15 @@
 package com.ami.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	private String home() {
+	private String home(Model model) {
+		model.addAttribute("navActive", "home");
 		return"home.html";
 
 	}
@@ -20,13 +21,15 @@ public class HomeController {
 	}
 	
 	@GetMapping("/user")
-	private String user() {
+	private String user(Model model) {
+		model.addAttribute("navActive", "user");
 		return"user.html";
 
 	}
 	
 	@GetMapping("/admin")
-	private String admin() {
+	private String admin(Model model) {
+		model.addAttribute("navActive", "admin");
 		return"admin.html";
 
 	}
