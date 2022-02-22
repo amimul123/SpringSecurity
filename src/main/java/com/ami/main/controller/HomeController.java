45ec6c7bd5e83f90f85d40193761a5646222ef86
amimulst <st158750@stud.uni-stuttgart.de>
2022-02-23 00:06:11 +0100

    username and role are added

--- src/main/java/com/ami/main/controller/HomeController.java
+++ src/main/java/com/ami/main/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ami.main.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +24,10 @@ public class HomeController {
 	
 	@GetMapping("/user")
 	private String user(Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		model.addAttribute("authentication",authentication);
 		model.addAttribute("navActive", "user");
+		
 		return"user.html";
 
 	}
